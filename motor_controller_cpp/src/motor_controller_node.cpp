2d78103a3c1f25cd7eafddb38eac8256ccb4adcc
RAVARY <guillian.ravary@gmail.com>
2025-05-13 13:59:18 +0200

    Réinitialisation de la branche main

--- motor_controller_cpp/src/motor_controller_node.cpp
+++
@@ -1,9 +0,0 @@
-#include "motor_controller.hpp"
-
-int main(int argc, char **argv) {
-    rclcpp::init(argc, argv);
-    auto node = std::make_shared<MotorController>();
-    rclcpp::spin(node);
-    rclcpp::shutdown();
-    return 0;
-}
